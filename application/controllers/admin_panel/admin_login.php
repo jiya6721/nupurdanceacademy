9045e69389ce8e7152aa7e484c6961be9035d1ba
jiya6721 <jiyagorasiya6721@gmail.com>
2025-01-03 12:04:49 +0530

    commit changes

---
+++ application/controllers/admin_panel/admin_login.php
@@ -0,0 +1,61 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');  
+
+class admin_login extends CI_Controller
+{
+	public function __construct()
+	{
+	parent::__construct();
+	$this->load->database('nda');
+	$this->load->helper('url');
+    $this->load->helper('form');
+ ///load model
+	}
+
+	
+        public function index()
+	{
+		if($this->input->post('login'))
+		{
+		$name=$this->input->post('name');
+		$email=$this->input->post('email');
+		$password=$this->input->post('password');
+
+		$this->Register_model->insert_data($name,$email,$password);
+
+	}else{
+        $this->load->view('admin_login_view.php');
+	}
+    }
+}
+?>
+
+
+
+<!-- <html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Login &mdash; Stisla</title>
+
+   General CSS Files -->
+  <!-- <link rel="stylesheet" href="public/modules/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="public/modules/fontawesome/css/all.min.css"> -->
+
+  <!-- CSS Libraries -->
+  <!-- <link rel="stylesheet" href="public/modules/bootstrap-social/bootstrap-social.css"> -->
+
+  <!-- Template CSS -->
+  <!-- <link rel="stylesheet" href="public/css/style.css"> -->
+  <!-- <link rel="stylesheet" href="public/css/components.css"> -->
+<!-- Start GA -->
+<!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script> -->
+<!-- <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-94034622-3');
+</script> -->
+<!-- /END GA --></head>
+</html>
